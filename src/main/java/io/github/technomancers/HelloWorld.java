7d8248bb3e689ca8bd114bf2736ccc120c604995
Will Dixon <dixonwille@gmail.com>
2017-04-30 17:00:35 -0400

    Adding JavaDocs

--- src/main/java/io/github/technomancers/HelloWorld.java
+++ src/main/java/io/github/technomancers/HelloWorld.java
@@ -1,5 +1,13 @@
 package io.github.technomancers;
+/**
+ * The Main-Class that is used as an entry point.
+ */
 public class HelloWorld {
+    /**
+     * Prints Hello World to screen.
+     * 
+     * @param args ignored in this case.
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
